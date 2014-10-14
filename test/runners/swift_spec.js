43583f6ac4abcdeed5e2e683a4be09e73ee9637e
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-10-14 15:02:06 -0700

    Only runs on a mac

--- test/runners/swift_spec.js
+++ test/runners/swift_spec.js
@@ -1,15 +1,17 @@
-var expect = require('chai').expect;
-var runner = require('../runner');
+if (process.platform == 'darwin') {
+    var expect = require('chai').expect;
+    var runner = require('../runner');
 
 
-describe( 'swift runner', function(){
-    describe( '.run', function(){
-        it( 'should handle basic code evaluation', function(done){
-            runner.run({language: 'swift', code: "println(\"You've baked a really lovely cake, but then you've used dog shit for frosting. - Steve Jobs (commenting on an employee's program)\")"}, function(buffer) {
-		console.log(buffer);
-                expect(buffer.stdout).to.equal("You've baked a really lovely cake, but then you've used dog shit for frosting. - Steve Jobs (commenting on an employee's program)\n");
-                done();
+    describe( 'swift runner', function(){
+	describe( '.run', function(){
+            it( 'should handle basic code evaluation', function(done){
+		runner.run({language: 'swift', code: "println(\"You've baked a really lovely cake, but then you've used dog shit for frosting. - Steve Jobs (commenting on an employee's program)\")"}, function(buffer) {
+		    console.log(buffer);
+                    expect(buffer.stdout).to.equal("You've baked a really lovely cake, but then you've used dog shit for frosting. - Steve Jobs (commenting on an employee's program)\n");
+                    done();
+		});
             });
-        });
+	});
     });
-});
+}
