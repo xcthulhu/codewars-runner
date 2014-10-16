e6750d228dd2e0534cbad3546e72381e5864fab4
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-10-16 15:31:13 -0700

    - No more aot compilation - Introducing ZMQ

--- jvm-runner/project.clj
+++ jvm-runner/project.clj
@@ -17,20 +17,19 @@
    [org.clojure/core.logic "0.8.8"] ; Logic Programming
    [org.clojure/core.match "0.2.1"] ; Pattern Matching
    [prismatic/plumbing "0.3.3"] ; Random Awesomeness
-   [slingshot "0.11.0"] ; Advanced Exceptions
    [clj-http "1.0.0"] ; HTTP Client
    [enlive "1.1.5"] ; HTML templating
    [hiccup "1.0.5"] ; Clojure HTML DSL
    [junit/junit "4.11"] ; JUnit
    [org.codehaus.groovy/groovy-all "2.3.6"] ; Groovy
    [environ "0.5.0"] ; Environment Variables
-   [com.keminglabs/zmq-async "0.1.0"] ; ZMQ over core-async
+   [org.zeromq/jeromq "0.3.3"] ; ZMQ
+   [org.zeromq/cljzmq "0.1.4" :exclusions [org.zeromq/jzmq]]
    ]
   :ring {:handler codewars.rest.handler/app}
   :plugins [[lein-environ "0.5.0"] [lein-ring "0.8.12"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
-  :env {:timeout "10000"}
-  :profiles {:uberjar {:aot :all}
-             :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.5.4"]]}}
+  :env {:timeout "10000"
+        :zmq-socket "tcp://127.0.0.1:12349"}
   :main codewars.core)
