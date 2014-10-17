fd012dfde28aaf6c541e5524e98c2e5160df6ee8
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-10-17 10:37:03 -0700

    Printing output

--- jvm-runner/src/clojure/codewars/runners.clj
+++ jvm-runner/src/clojure/codewars/runners.clj
@@ -15,11 +15,18 @@
           (format "Language %s is not implemented"
                   (pr-str language)))))
 
+(defn- print-result [{:keys [:stdout :stderr] :as result}]
+  (print stdout)
+  (binding [*out* *err*] (print stderr))
+  result)
+
 (defn run
   "Run code or a test-fixture."
   [opts]
-  (wrap-result
-   (with-timeout timeout
-     (if (contains? opts :fixture)
-       (full-project opts)
-       (code-only opts)))))
+  (->> (if (contains? opts :fixture)
+         (full-project opts)
+         (code-only opts))
+       (with-timeout timeout)
+       wrap-result
+       print-result
+       ))
