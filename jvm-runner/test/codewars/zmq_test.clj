a49b151d810e326de05da4a9917d4a00e12c845c
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2014-10-17 17:27:25 -0700

    Updating listen (so that it can work even when there's no stdin present)

--- jvm-runner/test/codewars/zmq_test.clj
+++ jvm-runner/test/codewars/zmq_test.clj
@@ -41,24 +41,25 @@
       (is (= {:result nil, :stderr "", :stdout "blorg\n"} @server)))))
 
 (deftest main-check
-  (let [main (future-call -main)]
-    (with-open
-        [req (doto (zmq/socket (zmq/zcontext) :req)
-               (zmq/bind codewars.zmq/addr))]
-      (testing "main wraps a ping"
-        (zmq/send-str req (json/generate-string "PONG"))
-        (is (= (json/parse-string (String. (zmq/receive req)))
-               {"type"   "status"
-                "status" "ready"
-                "message" "PONG"})))
-      (testing "main responds to a request"
-        (zmq/send-str req
-                      (json/generate-string
-                       {:language "java"
-                        :code "class AwesomeSauce { static int main(String [] args) {return 1;} }"}))
-        (is (= (json/parse-string (String. (zmq/receive req)))
-               {"type" "execution response",
-                "result" 1,
-                "stderr" "",
-                "stdout" ""})))
-      (is (= {:result 1, :stderr "", :stdout ""} @main)))))
+  (with-redefs [codewars.stdin/listen #(Thread/sleep 1000000)]
+    (let [main (future-call -main)]
+      (with-open
+          [req (doto (zmq/socket (zmq/zcontext) :req)
+                 (zmq/bind codewars.zmq/addr))]
+        (testing "main wraps a ping"
+          (zmq/send-str req (json/generate-string "PONG"))
+          (is (= (json/parse-string (String. (zmq/receive req)))
+                 {"type"   "status"
+                  "status" "ready"
+                  "message" "PONG"})))
+        (testing "main responds to a request"
+          (zmq/send-str req
+                        (json/generate-string
+                         {:language "java"
+                          :code "class AwesomeSauce { static int main(String [] args) {return 1;} }"}))
+          (is (= (json/parse-string (String. (zmq/receive req)))
+                 {"type" "execution response",
+                  "result" 1,
+                  "stderr" "",
+                  "stdout" ""})))
+        (is (= {:result 1, :stderr "", :stdout ""} @main))))))
